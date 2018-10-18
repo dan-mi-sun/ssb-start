e5917b7655ca7802268b91a312b070458ba25990
Dan Hassan <dan@dyne.org>
2018-10-18 11:10:45 +1100

    setting up simple ssb-query

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var connection = require('ssb-client')
+var pull = require('pull-stream')
 
-connection(function (error, server){
-  //anon fn cb within connection
+connection(function (error, server) {
+  // anon fn cb within connection
   if (error) console.log(`This is the error: ${error}`)
-  server.whoami(function (error, response){
-    if (error) console.log('Poo:', error)
-    console.log(response)
-    server.close()
-  })
+  var opts = { limit: 100, reverse: true }
 
+  var onDone = function (error, results) {
+    results.forEach(msg => {
+      console.log(msg.value.content)
+      console.log('----')
+    })
+    server.close()
+  }
+  pull(
+    server.query.read(opts), // the source
+    pull.filter(msg => msg.value.content.type === 'post'), // filter 'through'
+    pull.collect(onDone) // the sink
+  )
 })
